ef27f622061cf51e98ede2a25a61a4c540d0653c
Matheus Magno <matheusmagnon@gmail.com>
2022-10-14 11:57:17 -0300

    add colors to theme

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+}
+
+body{
+    /* font-size: 1.6rem;
+    line-height: 1.8rem; */
+    background:${(props) => props.theme["base-hover"]}
+} 
+
+`;
