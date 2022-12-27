93fc0233cbde22c5c71f30468ac4404f698e7e91
Matheus Magno <matheusmagnon@gmail.com>
2022-12-27 11:17:12 -0300

    fear: add immer

--- src/reducers/cartList/reducer.ts
+++ src/reducers/cartList/reducer.ts
@@ -1,3 +1,5 @@
+import { produce } from 'immer';
+
 import { ActionTypes } from './actions';
 
 export type CoffeType = {
@@ -23,33 +25,53 @@ export function cartListReducer(state: CartListState, action: any) {
       );
       //Se existir o mesmo café no carrinho seta o valor 1 no ItemsAmount
       if (coffeeAlreadyExistsInCart < 0) {
-        action.payload.newItem.itemsAmount = 1;
-        return {
-          ...state,
-          cartList: [...state.cartList, action.payload.newItem],
-        };
+        const newItem = { ...action.payload.newItem };
+        newItem.itemsAmount = 1;
+        // return {
+        //   ...state,
+        //   cartList: [...state.cartList, action.payload.newItem],
+        // };
+        return produce(state, (draft) => {
+          draft.cartList.push(newItem);
+        });
         //Se não retorna com o atributo do objeto alterado
       } else {
-        return {
-          ...state,
-          cartList: state.cartList.map((coffeInCart) => {
-            if (coffeInCart.id === action.payload.newItem.id) {
-              const contItems = coffeInCart.itemsAmount;
-              return { ...coffeInCart, itemsAmount: contItems + 1 };
-            } else {
-              return coffeInCart;
-            }
-          }),
-        };
+        // return {
+        //   ...state,
+        //   cartList: state.cartList.map((coffeInCart) => {
+        //     if (coffeInCart.id === action.payload.newItem.id) {
+        //       const contItems = coffeInCart.itemsAmount;
+        //       return { ...coffeInCart, itemsAmount: contItems + 1 };
+        //     } else {
+        //       return coffeInCart;
+        //     }
+        //   }),
+        // };
+        const currentCartItem = state.cartList.findIndex((item) => {
+          return item.id === action.payload.newItem.id;
+        });
+
+        const contItems = action.payload.newItem.itemsAmount;
+
+        return produce(state, (draft) => {
+          draft.cartList[currentCartItem].itemsAmount = contItems + 1;
+        });
       }
     case ActionTypes.DELETE_ITEM:
-      if (action.payload.itemToDelete.itemsAmount == 1) {
+      console.log(action.payload.itemToDelete.itemsAmount === 1);
+
+      if (action.payload.itemToDelete.itemsAmount === 1) {
         return {
           ...state,
           cartList: state.cartList.filter(
             (coffe) => coffe.id !== action.payload.itemToDelete.id,
           ),
         };
+        // return produce(state, (draft) => {
+        //   draft.cartList.filter(
+        //     (coffe) => coffe.id !== action.payload.itemToDelete.id,
+        //   );
+        // });
       } else {
         return {
           ...state,
